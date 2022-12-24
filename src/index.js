96727be55da8a526ad1d278bc15249af364450be
rubenlb99 <rubenlopez6690@gmail.com>
2022-12-24 18:57:59 +0100

    added cards and css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import ReactDOM  from "react-dom"
 import { Provider } from 'react-redux'
-import { applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { configureStore } from '@reduxjs/toolkit';
+import { compose } from 'redux';
+
 import rootReducer from './reducers';
 
 import App from './App'
+import './index.css'
+
 
 const store = configureStore({
-    reducer: rootReducer,
-    middleware: compose(applyMiddleware(thunk))
-  });  
+  reducer: rootReducer,
+  middleware: [compose(thunk)],
+});  
 
 ReactDOM.render(
     <Provider store={store}>
